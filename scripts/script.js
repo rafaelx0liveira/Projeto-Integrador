586fe9a1af4dba29df297c0c7f2b8f63364a58d1
rafael <rafaelaparecido.oliveirasilva@gmail.com>
2022-12-14 20:57:13 -0300

    Ajustando menu hamburguer

--- scripts/script.js
+++ scripts/script.js
@@ -24,7 +24,7 @@ const btnMobile = document.getElementById("btn-mobile");
 function toggleMenu(event) {
   if (event.type === "touchstart") event.preventDefault();
 
-  const nav = document.getElementById("navigation");
+  const nav = document.getElementById("nav");
   nav.classList.toggle("active");
   const active = nav.classList.contains("active");
   event.currentTarget.setAttribute("aria-expanded", active);
