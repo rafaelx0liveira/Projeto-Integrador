a4f9e201d2c1c1470a7a6bab4ed603733354b730
Henrique Fornazeiro <henrique.fornazeiro@hotmail.com>
2023-01-27 11:54:02 -0300

    Criando rota e controller para verificacao de idad

--- src/controllers/HomeController.js
+++ src/controllers/HomeController.js
@@ -1,6 +1,12 @@
 const homeController = {
-    showHome: (req,res) =>{
-        res.render('index')
-    }
-}
-module.exports = homeController
+  showCheckAge: (req, res) => {
+    res.render("checagem_idade");
+  },
+  showAgeDeclined: (req, res) => {
+    res.render("idade_recusada");
+  },
+  showHome: (req, res) => {
+    res.render("index");
+  },
+};
+module.exports = homeController;
