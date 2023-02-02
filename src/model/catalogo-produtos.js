f3591ecce746e24a8ab48d053cb05f85916e93ee
rafael <rafaelaparecido.oliveirasilva@gmail.com>
2023-02-02 12:44:00 -0300

    Renderizando produtos nas paginas

--- src/model/catalogo-produtos.js
+++ src/model/catalogo-produtos.js
@@ -5,6 +5,8 @@ const path = require('path');
 const crypto = require('crypto');
 
 const catalogo = {
+
+  //Função para buscar todos os produtos
   findAll: () => {
     const produtos = [];
 
@@ -21,6 +23,23 @@ const catalogo = {
     });
 
     return produtos;
+  },
+
+  //Função para buscar um produto pelo id
+  findByPk: (id) => {
+    const produto = db.products.find(produto => produto.id == id);
+
+    const produtoFormatado = {
+      id: produto.id,
+      nome: produto.name,
+      descricao: produto.description,
+      preco: `R$ ${produto.price.toLocaleString("pt-BR", { currency: "BRL", minimumFractionDigits: 2 })}`,
+      imagem: produto.image,
+      estoque: produto.stock,
+      alcoolico: produto.alcoholic
+    };
+
+    return produtoFormatado;
   }
 }
 
