0ea393d86e7c546e8c45dd850d2f4c7a607c4f46
rafael <rafaelaparecido.oliveirasilva@gmail.com>
2023-02-28 19:14:56 -0300

    Inserindo script de criação do banco

--- src/database/script_pi.sql
+++ src/database/script_pi.sql
@@ -1,62 +1,49 @@
 -- MySQL Workbench Forward Engineering
 
-SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0;
-SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0;
-SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='ONLY_FULL_GROUP_BY,STRICT_TRANS_TABLES,NO_ZERO_IN_DATE,NO_ZERO_DATE,ERROR_FOR_DIVISION_BY_ZERO,NO_ENGINE_SUBSTITUTION';
-
 -- -----------------------------------------------------
--- Schema mydb
+--        SCRIPT DE CRIAÇÃOD DO BANCO DE DADOS
 -- -----------------------------------------------------
 
+USE `pi_dh` ;
 -- -----------------------------------------------------
--- Schema mydb
+-- Table `pi_dh`.`Produto`
 -- -----------------------------------------------------
-CREATE SCHEMA IF NOT EXISTS `mydb` DEFAULT CHARACTER SET utf8 ;
--- -----------------------------------------------------
--- Schema banco_pi
--- -----------------------------------------------------
-USE `mydb` ;
-
--- -----------------------------------------------------
--- Table `mydb`.`Produto`
--- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `mydb`.`Produto` (
-  `idProduto` INT NOT NULL AUTO_INCREMENT,
-  `nome` VARCHAR(100) NOT NULL,
-  `descricao` VARCHAR(250) NOT NULL,
-  `preco` DECIMAL NOT NULL,
-  `alcoolico` TINYINT NOT NULL,
-  `imagem` VARCHAR(100) NOT NULL,
-  `ativo` TINYINT NOT NULL,
-  `estoque` TINYINT NOT NULL,
-  `ingredientes` VARCHAR(250) NOT NULL,
-  `harmonizacao` VARCHAR(300) NOT NULL,
-  `graduacao_alcoolica` DECIMAL NOT NULL,
-  `volume` DECIMAL NOT NULL,
-  PRIMARY KEY (`idProduto`))
+CREATE TABLE Produto(
+ `idProduto` INT NOT NULL ,
+ `nome` VARCHAR(100) NOT NULL,
+ `descricao` VARCHAR(250) NOT NULL,
+ `preco` DECIMAL NOT NULL,
+ `alcoolico` TINYINT NOT NULL,
+ `imagem` VARCHAR(100) NOT NULL,
+ `ativo` TINYINT NOT NULL,
+ `estoque` TINYINT NOT NULL,
+ `ingredientes` VARCHAR(250) NOT NULL,
+ `harmonizacao` VARCHAR(300) NOT NULL,
+ `graduacao_alcoolica` DECIMAL NOT NULL,
+ `volume` DECIMAL NOT NULL,
+ PRIMARY KEY (`idProduto`))
 ENGINE = InnoDB;
 
+-- -----------------------------------------------------
+-- Table `pi_dh`.`Usuario`
+-- -----------------------------------------------------
 
--- -----------------------------------------------------
--- Table `mydb`.`Usuario`
--- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `mydb`.`Usuario` (
-  idUsuario INT NOT NULL,
-  nome VARCHAR(100) NOT NULL,
-  email VARCHAR(50) NOT NULL,
-  cpf VARCHAR(11) NOT NULL,
-  telefone VARCHAR(45) NOT NULL,
-  dtNascimento DATE NOT NULL,
-  PRIMARY KEY (idUsuario),
-  UNIQUE INDEX `email_UNIQUE` (email ASC) VISIBLE,
-  UNIQUE INDEX `cpf_UNIQUE` (cpf ASC) VISIBLE)
-ENGINE = InnoDB;
+CREATE TABLE `usuario` (
+  `idUsuario` int(11) NOT NULL,
+  `nome` varchar(100) NOT NULL,
+  `email` varchar(50) NOT NULL,
+  `cpf` varchar(11) NOT NULL,
+  `telefone` varchar(45) NOT NULL,
+  `dtNascimento` date NOT NULL,
+  PRIMARY KEY (`idUsuario`)
+) ENGINE=InnoDB;
 
 
 -- -----------------------------------------------------
--- Table `mydb`.`Endereco`
+-- Table `pi_dh`.`Endereco`
 -- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `mydb`.`Endereco` (
+
+CREATE TABLE Endereco (
   `idEndereco` INT NOT NULL,
   `rua` VARCHAR(100) NOT NULL,
   `cep` VARCHAR(8) NOT NULL,
@@ -65,40 +52,30 @@ CREATE TABLE IF NOT EXISTS `mydb`.`Endereco` (
   `cidade` VARCHAR(50) NOT NULL,
   `complemento` VARCHAR(50) NOT NULL,
   `Usuario_idUsuario` INT NOT NULL,
-  PRIMARY KEY (`idEndereco`, `Usuario_idUsuario`),
-  INDEX `fk_Endereco_Usuario1_idx` (`Usuario_idUsuario` ASC) VISIBLE,
-  CONSTRAINT `fk_Endereco_Usuario1`
-    FOREIGN KEY (`Usuario_idUsuario`)
-    REFERENCES `mydb`.`Usuario` (`idUsuario`)
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION)
+  PRIMARY KEY (`idEndereco`, `Usuario_idUsuario`))
 ENGINE = InnoDB;
 
 
 -- -----------------------------------------------------
--- Table `mydb`.`Pagamentos`
+-- Table `pi_dh`.`Pagamentos`
 -- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `mydb`.`Pagamentos` (
+
+CREATE TABLE IF NOT EXISTS Pagamentos (
   `idPagamentos` INT NOT NULL,
   `cartao` VARCHAR(20) NOT NULL,
   `validade` DATE NOT NULL,
   `cv` INT(3) NOT NULL,
   `nome` VARCHAR(50) NOT NULL,
   `Usuario_idUsuario` INT NOT NULL,
-  PRIMARY KEY (`idPagamentos`, `Usuario_idUsuario`),
-  INDEX `fk_Pagamentos_Usuario1_idx` (`Usuario_idUsuario` ASC) VISIBLE,
-  CONSTRAINT `fk_Pagamentos_Usuario1`
-    FOREIGN KEY (`Usuario_idUsuario`)
-    REFERENCES `mydb`.`Usuario` (`idUsuario`)
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION)
+  PRIMARY KEY (`idPagamentos`, `Usuario_idUsuario`))
 ENGINE = InnoDB;
 
 
 -- -----------------------------------------------------
--- Table `mydb`.`Pedido`
+-- Table `pi_dh`.`Pedido`
 -- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `mydb`.`Pedido` (
+
+CREATE TABLE IF NOT EXISTS Pedido (
   `idPedido` INT NOT NULL,
   `valor_total` DECIMAL NOT NULL,
   `data_pedido` DATE NOT NULL,
@@ -106,44 +83,17 @@ CREATE TABLE IF NOT EXISTS `mydb`.`Pedido` (
   `status` ENUM('em andamento', 'pago', 'expirado', 'cancelado') NOT NULL,
   `Usuario_idUsuario` INT NOT NULL,
   `Pagamentos_idPagamentos` INT NOT NULL,
-  PRIMARY KEY (`idPedido`, `Usuario_idUsuario`, `Pagamentos_idPagamentos`),
-  INDEX `fk_Pedido_Usuario1_idx` (`Usuario_idUsuario` ASC) VISIBLE,
-  INDEX `fk_Pedido_Pagamentos1_idx` (`Pagamentos_idPagamentos` ASC) VISIBLE,
-  CONSTRAINT `fk_Pedido_Usuario1`
-    FOREIGN KEY (`Usuario_idUsuario`)
-    REFERENCES `mydb`.`Usuario` (`idUsuario`)
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION,
-  CONSTRAINT `fk_Pedido_Pagamentos1`
-    FOREIGN KEY (`Pagamentos_idPagamentos`)
-    REFERENCES `mydb`.`Pagamentos` (`idPagamentos`)
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION)
+  PRIMARY KEY (`idPedido`, `Usuario_idUsuario`, `Pagamentos_idPagamentos`))
 ENGINE = InnoDB;
 
 
 -- -----------------------------------------------------
--- Table `mydb`.`Pedido_has_Produto`
+-- Table `pi_dh`.`Pedido_has_Produto`
 -- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `mydb`.`Pedido_has_Produto` (
+
+CREATE TABLE IF NOT EXISTS Pedido_has_Produto (
   `Pedido_idPedido` INT NOT NULL,
   `Produto_idProduto` INT NOT NULL,
-  PRIMARY KEY (`Pedido_idPedido`, `Produto_idProduto`),
-  INDEX `fk_Pedido_has_Produto_Produto1_idx` (`Produto_idProduto` ASC) VISIBLE,
-  INDEX `fk_Pedido_has_Produto_Pedido_idx` (`Pedido_idPedido` ASC) VISIBLE,
-  CONSTRAINT `fk_Pedido_has_Produto_Pedido`
-    FOREIGN KEY (`Pedido_idPedido`)
-    REFERENCES `mydb`.`Pedido` (`idPedido`)
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION,
-  CONSTRAINT `fk_Pedido_has_Produto_Produto1`
-    FOREIGN KEY (`Produto_idProduto`)
-    REFERENCES `mydb`.`Produto` (`idProduto`)
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION)
+  PRIMARY KEY (`Pedido_idPedido`, `Produto_idProduto`))
 ENGINE = InnoDB;
 
-
-SET SQL_MODE=@OLD_SQL_MODE;
-SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS;
-SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS;
