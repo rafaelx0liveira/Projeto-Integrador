81c7595c243928f8df470ae5b479e9f2bba7e824
Henrique Fornazeiro <henrique.fornazeiro@hotmail.com>
2023-03-02 18:53:33 -0300

    finalizando views, rotas e controllers de admin

--- src/controllers/AdminController.js
+++ src/controllers/AdminController.js
@@ -1,12 +1,30 @@
 const AdminController = {
     showLoginAdmin: (req, res)=>{
-        res.render("admin/login");
+        res.render("admin/login/index");
     },
-    showDashAdmin: (req, res)=>{
+    showIndex: (req, res)=>{
         res.render("admin/index");
     },
-    showProdutosAdmin: (req, res)=>{
-        res.render("admin/produtos");
+    showPedidos: (req, res)=>{
+        res.render("admin/pedidos/index");
+    },
+    showDetalhePedido: (req, res)=>{
+        res.render("admin/pedidos/detalhePedido");
+    },
+    showProdutos: (req, res)=>{
+        res.render("admin/produtos/index");
+    },
+    showEditarProduto: (req, res)=>{
+        res.render("admin/produtos/editarProduto");
+    },
+    showCadastrarProduto: (req, res)=>{
+        res.render("admin/produtos/cadastroProduto");
+    },
+    showClientes: (req, res)=>{
+        res.render("admin/clientes/index");
+    },
+    showEditarCliente: (req, res)=>{
+        res.render("admin/clientes/editarCliente");
     },
 }
 
--- src/routes/AdminRouter.js
+++ src/routes/AdminRouter.js
@@ -3,9 +3,14 @@ const router = express.Router();
 const AdminController = require("../controllers/AdminController");
 
 router.get("/login", AdminController.showLoginAdmin);
-router.get("/dashboard", AdminController.showDashAdmin);
-router.get("/produtos", AdminController.showProdutosAdmin);
-
+router.get("/index", AdminController.showIndex);
+router.get("/pedidos", AdminController.showPedidos);
+router.get("/detalhePedido", AdminController.showDetalhePedido);
+router.get("/produtos", AdminController.showProdutos);
+router.get("/produtos/editar", AdminController.showEditarProduto);
+router.get("/produtos/cadastro", AdminController.showCadastrarProduto);
+router.get("/clientes", AdminController.showClientes);
+router.get("/clientes/editar", AdminController.showEditarCliente);
 
 
 module.exports = router;
