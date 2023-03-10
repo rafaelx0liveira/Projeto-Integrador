b3afd70620094edaf6147691b9faea7e9a2b6352
rafael <rafaelaparecido.oliveirasilva@gmail.com>
2023-03-10 19:13:42 -0300

    Ajustando catalogo

--- src/database/migrations/20230303124839-create-table_produtos.js
+++ src/database/migrations/20230303124839-create-table_produtos.js
@@ -4,7 +4,7 @@
 module.exports = {
   async up (queryInterface, Sequelize) {
 
-     await queryInterface.createTable('produto', { 
+    await queryInterface.createTable('produto', { 
       id: {
         type: Sequelize.INTEGER,
         autoIncrement: true,
