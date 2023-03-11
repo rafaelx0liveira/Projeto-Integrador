3e1458475166e154580cbefb332322b8655968d3
Henrique Fornazeiro <henrique.fornazeiro@hotmail.com>
2023-03-10 21:45:38 -0300

    Ajustes no controller e na view para mostrar produtos na página de catálogo

--- src/controllers/HomeController.js
+++ src/controllers/HomeController.js
@@ -1,9 +1,9 @@
-// const catalogo = require("../database/old/catalogo-produtos");
 const {Produto} = require("../model")
 
 const homeController = {
   showHome: async (req, res) => {
-    //Buscando os produtos no model
+    //Buscando os produtos no banco
+
     const produtosTop = await Produto.findAll({ limit: 15 });
     const produtosCerveja = await Produto.findAll({ where: {
       tipo: "cerveja"
@@ -11,19 +11,43 @@ const homeController = {
     const produtosUisque = await Produto.findAll({ where: {
       tipo: "uisque"
     }, limit: 10 })
-    //Renderizando a view catalogo e passando os produtos
+
+    //Renderizando a view index e passando os produtos
     res.render("index", { produtosTop,produtosCerveja,produtosUisque });
   },
+
   showLojas: (req, res) => {
     res.render("lojas");
   },
+  
   showCatalogo: async(req, res) => {
-    //Buscando os produtos no model
-    const produtos = await Produto.findAll();
+    //paginação
+    //valor fixo para página para inicar 
+    let totalPorPagina = 15
+    let { page = 1 } = req.query;
+    
+    if(page < 1){
+      page = 1
+    }
 
+    let prevPage = Number(page) - Number(1);
+    let nextPage = Number(page) + Number(1);
+
+    //busca o total de registros
+    let { count: total, rows: produtos } = await Produto.findAndCountAll({
+      where:{
+        ativo: 1
+      },
+      limit: totalPorPagina,
+      offset: (page - 1) * totalPorPagina,
+    });
+
+    let totalPagina = Math.ceil(total / totalPorPagina);
+    
     //Renderizando a view catalogo e passando os produtos
-    res.render("catalogo", { produtos });
+    res.render("catalogo", { produtos, totalPagina, page, prevPage, nextPage});
   },
+
   showContato: (req, res) => {
     res.render("contato");
   },
--- src/controllers/ProductController.js
+++ src/controllers/ProductController.js
@@ -1,12 +1,11 @@
-// const catalogo = require('../model/catalogo-produtos');
+const {Produto} = require("../model")
 
 const productRouter = {
-    showProduct: (req,res) =>{
+    showProduct: async (req,res) =>{
         const id = req.params.id;
-        // const produto = catalogo.findByPk(id);
-        // const produtos = catalogo.findAll();
-
-        res.render('produto');
+        const produto = await Produto.findByPk(id);
+        console.log(produto)
+        res.render('produto', {produto});
     }
 }
 
